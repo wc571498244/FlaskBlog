f1c89168f0d1c6e7df9dee722b0e5989a53dcfbb
wc <571498244@qq.com>
2019-08-27 20:51:52 +0800

    添加文章评论模型和ajax提交评论

--- FlaskBlog/App/models.py
+++ FlaskBlog/App/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from .exts import db
 
 
@@ -25,10 +27,20 @@ class Article(db.Model):
     description = db.Column(db.String(200))
     img = db.Column(db.String(255), default="")
     article_type = db.Column(db.Integer, db.ForeignKey(ArticleType.id))
-
+    comments = db.relationship("Comment", backref="article1", lazy=True)
 
 # 标签表
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
     articles = db.relationship('Article', backref='tag1', secondary=article_tags, lazy=True)
+
+
+
+# 评论表
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(300))
+    time = db.Column(db.DateTime, default=datetime.datetime.now())
+    username = db.Column(db.String(30))
+    article_id = db.Column(db.Integer, db.ForeignKey(Article.id))
--- FlaskBlog/App/views.py
+++ FlaskBlog/App/views.py
@@ -1,23 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from sqlalchemy import desc
+
 from .models import *
 
 blog = Blueprint('blog', __name__)
 admin = Blueprint('admin', __name__)
 
 
+def get_same_info():
+    # 文章分类
+    article_type = ArticleType.query.filter().all()
+    # 站长推荐
+    main_articles = Article.query.order_by(desc("click_num")).all()
+    data = {
+        "article_type": article_type,
+        "main_articles": main_articles,
+    }
+    return data
+
+
 # 博客首页
 @blog.route('/')
 def index():
-    articles = Article.query.filter().all()
-    article_type = ArticleType.query.filter().all()
+    articles = Article.query.order_by(desc("create_time")).all()
     data = {
         "articles": articles,
         "title": "首页",
-        "article_type": article_type,
     }
-    print("-----------" * 20)
-    for i in article_type:
-        print(len(i.articles))
+    data.update(get_same_info())
     return render_template("blog/index.html", **data)
 
 
@@ -25,14 +35,16 @@ def index():
 @blog.route('/detail/<int:id>/')
 def detail(id):
     article = Article.query.get(id)
-    article_type = ArticleType.query.filter().all()
     tags = article.tag1
+    # 评论列表
+    comments = Comment.query.filter_by(article_id=id).all()
     data = {
         "title": "文章详情",
         "article": article,
         "tags": tags,
-        "article_type": article_type,
+        "comments": comments
     }
+    data.update(get_same_info())
     return render_template("blog/detail.html", **data)
 
 
@@ -40,10 +52,58 @@ def detail(id):
 @blog.route("/typelist/<int:id>/")
 def typelist(id):
     articles = ArticleType.query.get(id).articles
-    article_type = ArticleType.query.filter().all()
     data = {
         "articles": articles,
         "title": "首页",
-        "article_type": article_type,
     }
+    data.update(get_same_info())
     return render_template("blog/typelist.html", **data)
+
+
+# 文章评论
+@blog.route('/article/comment/', methods=['POST'])
+def article_comment():
+    username = request.form.get("username")
+    saytext = request.form.get("saytext")
+    article_id = request.form.get("article_id")
+    if username == "" or saytext == "":
+        data = {
+            "code": 1001,
+            "msg": "用户名和内容不能为空"
+        }
+        return jsonify(data)
+    comment = Comment.query.filter_by(username=username).first()
+    if not comment:
+        comment = Comment()
+        comment.username = username
+        comment.content = saytext
+        comment.article_id = article_id
+        db.session.add(comment)
+        db.session.commit()
+        data = {
+            "code": 1000,
+            "msg": "评论成功",
+            "data": {
+                "username": comment.username,
+                "content": comment.content,
+                "time": comment.time,
+            }
+        }
+        return jsonify(data)
+    data = {
+        "code": 1002,
+        "msg": "用户名已存在"
+    }
+    return jsonify(data)
+
+
+# 后台管理首页
+@admin.route("/admin/index/")
+def admin_index():
+    return render_template("admin/index.html")
+
+
+# 后台文章管理
+@admin.route("/admin/article/")
+def admin_article():
+    return render_template("admin/article.html")
